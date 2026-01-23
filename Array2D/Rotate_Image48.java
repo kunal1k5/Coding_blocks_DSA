60ae162ab9bc1c46458d8998c980987a9db751e1
kunal <kunalkumar151123@gmail.com>
2026-01-23 10:24:44 +0530

    Restore Rotate_Image48 (rotate 90 degree) problem

---
+++ Array2D/Rotate_Image48.java
@@ -0,0 +1,37 @@
+package Array2D;
+
+public class Rotate_Image48 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void RotateImage(int[][] arr){
+
+        for(int i=0; i<arr.length; i++){
+            for(int j=i+1; j<arr[0].length; j++){
+
+                int temp = arr[j][i];
+                arr[j][i] = arr[i][j];
+                arr[i][j] = temp;
+            }
+        }
+
+        for(int i=0; i<arr.length; i++){
+            reverse(arr[i], 0, arr[0].length-1);
+        }
+
+    }
+
+    public static void reverse(int[] arr,int i, int j){       
+
+         while(i<j){
+
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+         }
+    }
+}
