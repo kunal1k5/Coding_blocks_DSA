7b8cd17800b91c758a7b5136aae6b89e5c0144dd
kunal1k5 <kunalkumar151123@gmail.com>
2026-02-05 23:02:45 +0530

    new question

---
+++ Recursion/fibonacci.java
@@ -0,0 +1,19 @@
+public class fibonacci {
+
+    public static void main(String[] args) {
+        int n = 4;
+        System.out.println(fibo(n));
+    }
+
+    public static int fibo(int n){
+
+        if(n==0 || n==1){
+            return n;
+        }
+
+        int f1 = fibo(n-1);
+        int f2 = fibo(n-2);
+        return f1+f2;
+    }
+    
+}
